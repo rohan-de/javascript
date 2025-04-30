d84fe8192bbec821433aff9e30a1258c6f03e834
Rohan <149697636+rohan-de@users.noreply.github.com>
2025-04-30 20:57:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+// document.getElementById("myH1").textContent = `Hello`;
+// document.getElementById("myp").textContent = `I am Rohan`;
+// window.alert(`Welcome`);
+
+//Variable = A container that storesa value Behaves as if it were the value it contains.
+
+//1. declaration         let x;
+//2. assignment          x = 100;
+
+// Numbers
+// let age = 22;
+// let price = 20.55;
+// let gpa = 2.1;
+
+
+//strings
+
+// let firstName = "Rahul";
+// let favoriteFood = "Roll";
+
+
+//Booleans ( Booleans equal true or false).
+
+let fullName = "Rohan de";
+let age = "22";
+let student = "false";
+
+document.getElementById("p1").textContent = `I am ${fullName}`;
+document.getElementById("p2").textContent = `I am ${age} year's old`;
+document.getElementById("p3").textContent = `I am student : ${student}`;
+
+
+
