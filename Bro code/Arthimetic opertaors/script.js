81dca9c80c3cbad661e473c490322eeb4054af6f
Rohan <149697636+rohan-de@users.noreply.github.com>
2025-05-03 21:44:07 +0530

    Add files via upload

---
+++ Bro code/Arthimetic opertaors/script.js
@@ -0,0 +1,51 @@
+
+//arthimetic operatprs = operands  (values, variables, etc.)
+//                       operators (+ - * /)
+//                       ex. 11 = x+5;
+// Operators Stands for 
+//                    Addition (+), Subtraction (-), Multiplication(*) and Divison(/)
+//                    Exponents(ex), Asterisks(**).
+
+
+//let students = 30;
+
+//students = students + 1;
+//students = students - 1;
+//students = students * 2;
+//students = students / 2;
+//students = students ** 3;   //(Double asterisks operator)
+//students = students % 2;    //(Modulus operator)
+//let extraStudents = students % 3;
+
+
+//Augmentea assignment operators
+
+//students += 1;
+//students -= 1;
+//students *= 2;
+//students /= 2;
+//students **= 2;
+//students %= 2;
+
+//increment / decrement operator
+
+//students++;
+//students--;
+
+
+//Operator Precedence
+
+/*
+    opertor precedence
+    1. parenthesis ()
+    2. expoents
+    3. multiplication & division & modulo
+    4. addition & subtraction
+*/
+
+//let result = 1 + 2 * 3 /*(second)*/ + 4 ** 2 //(first);
+
+//let result = 12 % 5 /* first */  + 8 / 2;
+
+//let result = 6 / 2 ** (2 + 5)/* firts*/; 
+
---
+++ Bro code/JavaScript input/script.js
@@ -0,0 +1,5 @@
+// How to accept user input 
+
+// 1. EASY WAY = window prompt
+// 2. PROFESSIONAL WAY = HTML textbox
+
