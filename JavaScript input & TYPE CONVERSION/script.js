8e558605200d1f138b209a6b5fec8be941b5e4ca
Rohan <149697636+rohan-de@users.noreply.github.com>
2025-05-12 21:15:58 +0530

    Add files via upload

---
+++ JavaScript input & TYPE CONVERSION/script.js
@@ -0,0 +1,48 @@
+// How to accept user input 
+
+// 1. EASY WAY = window prompt
+// 2. PROFESSIONAL WAY = HTML textbox
+
+
+// 1. WINDOW PROMPT
+
+// let username;
+// username = window.prompt("What's your username?");
+// console.log(username);
+
+
+// 2. HTML textbox
+
+// let username;
+
+// document.getElementById("mySubmit").onclick = function () {
+//     username = document.getElementById("myText").value;
+//     document.getElementById("myh1").textContent = `Hello ${username}`
+// }
+
+
+
+
+
+
+
+
+
+
+
+// type conversion = change the datatype of a value to another
+//                    (string, numbers, booleans)
+
+// Example
+
+// let x;
+// let y;
+// let z;
+
+// x = Number(x);
+// y = String(y);
+// z = Boolean(z);
+
+// console.log(x, typeof x);
+// console.log(y, typeof y);
+// console.log(z, typeof z);
